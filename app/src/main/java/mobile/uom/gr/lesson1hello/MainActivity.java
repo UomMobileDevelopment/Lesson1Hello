418a2bbbaa122799ebe7d2ae88de1514d6fbed08
Theodore Chaikalis <teohaik@gmail.com>
2016-10-07 20:23:57 +0300

    implementation of foo method for button click handling

--- app/src/main/java/mobile/uom/gr/lesson1hello/MainActivity.java
+++ app/src/main/java/mobile/uom/gr/lesson1hello/MainActivity.java
@@ -2,6 +2,7 @@ package mobile.uom.gr.lesson1hello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //show message method
+    public void foo(View view){
+        System.out.println("Button clicked!");
     }
 }
